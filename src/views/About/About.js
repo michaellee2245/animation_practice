bc7f41ce490d418ea205a4ccb40b96d238a9cbe4
Michael Lee <michaellee2245@gmail.com>
2019-11-19 10:12:13 -0700

    added small animations on home and about page for practice

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,17 +1,108 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './about.scss';
+import { TweenMax, Power3 } from 'gsap';
 
-class About extends Component {
-    render() {
-        return (
-            <div className="about-container">
-                <div className="text">The about us page</div>
-                <Link to="/"><button>Return Home</button></Link>
-                <Link to="/loader"><button>The Loader</button></Link>
-            </div>
-        );
+function About() {
+    let circleContainer = useRef(null)
+    let homeBtn = useRef(null);
+    let loaderBtn = useRef(null)
+    let circle = useRef(null)
+    let circleRed = useRef(null)
+    let circleBlue = useRef(null)
+
+    const [state, setState] = useState(false)
+
+    const handleExpand = () => {
+        TweenMax.to(circleRed, 1.5, {width: 200, height: 200, ease:Power3.easeOut})
+        setState(true)
     }
+    const handleShrink = () => {
+        TweenMax.to(circleRed, 1.5, {width: 75, height: 75, ease:Power3.easeOut})
+        setState(false)
+    }
+
+    useEffect(() => {
+        TweenMax.to(
+            homeBtn,
+            1.5,
+            {
+                opacity: 1,
+                y: -20,
+                ease: Power3.easeOut
+            }
+        )
+        TweenMax.to(
+            loaderBtn,
+            1.5,
+            {
+                opacity: 1,
+                y: -20,
+                ease: Power3.easeOut,
+                delay: .5,
+            }
+        )
+      
+        TweenMax.to(
+            circleContainer,
+            0,
+            {
+                css: {visibility: 'visible'}
+            }
+        )
+
+        TweenMax.from(
+            circle,
+            1.5,
+            {
+                opacity:0,
+                x:40,
+                ease:Power3.easeOut
+            }
+        )    
+        TweenMax.from(
+            circleRed,
+            1.5,
+            {
+                opacity:0,
+                x:40,
+                ease:Power3.easeOut,
+                delay:.2
+            }
+        )    
+        TweenMax.from(
+            circleBlue,
+            1.5,
+            {
+                opacity:0,
+                x:40,
+                ease:Power3.easeOut,
+                delay:.4
+            }
+        )    
+
+    }, [])
+
+    return (
+        <div className="about-container" >
+            <div className="text">The about us page</div>
+            <Link to="/"><button ref={el => { homeBtn = el }}>Return Home</button></Link>
+            <Link to="/loader"><button ref={el => loaderBtn = el}>The Loader</button></Link>
+
+            <div className="circle-container" ref={el => circleContainer = el}>
+                <div className="circle"
+                    ref={el => circle = el}></div>
+
+                <div className="circle red"
+                    ref={el => circleRed = el}
+                    onClick={state !== true ? handleExpand : handleShrink}></div>
+
+                <div className="circle blue"
+                    ref={el => circleBlue = el}></div>
+            </div>
+        </div>
+    );
+
 }
 
 export default About;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './home.scss';
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
+import { TimelineLite, Power2 } from 'gsap';
 
-class Home extends Component {
-    render() {
-        return (
-            <div className="home-container">
-                <div className="text">The Home Page</div>
-                <Link to='/about'><button>Learn more</button></Link>
+import coolPhoto from '../../images/alex-talmon-UFskvng2OMU-unsplash.jpg'
+
+const Home = () => {
+    let imgSection = useRef(null);
+    let image = useRef(null);
+    let imageReveal = CSSRulePlugin.getRule(".img-container:after")
+
+    const tl = new TimelineLite()
+
+    useEffect(() => {
+        tl.to(imgSection, 0, { css: { visibility: "visible" },})
+        .to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut, delay: 2 })
+        .from(image, 1.4, {scale: 1.6, ease: Power2.easeInOut, delay: -1.0});
+    });
+
+    return (
+        <div className="home-container">
+            <div className="text">The Home Page</div>
+            <Link to='/about'><button>Learn more</button></Link>
+
+            <div className="img-section" ref={el => (imgSection = el)}>
+                <>
+                    <div className="img-container">
+                        <img src={coolPhoto}
+                            ref={el => (image = el)}
+                        />
+                    </div>
+                </>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Home;
